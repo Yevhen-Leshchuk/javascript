cf84664dc17dc70251f1cdbd8eea18e7dba6633a
yevhen leshchuk <evg.leschuk@gmail.com>
2021-03-06 22:48:46 +0200

    added example 01-vars

---
+++ js/js-01/01-vars.js
@@ -0,0 +1,17 @@
+// Переменные
+
+const age = 10;
+const firstName = 'Chelsy';
+const isVIP = true;
+const secondName = 'Manchester United';
+const type = typeof 5;
+
+console.log(age);
+console.log(firstName);
+console.log(isVIP);
+console.log(secondName);
+console.log(type);
+console.log('This is a number:', age);
+console.log('Before');
+alert('Hello');
+console.log('After');
---
+++ js/js-01/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let hobby = 'Шашки';
+
+hobby = 'Рыбалка'
+console.log(hobby);
+
+const message = 'Welkome to Riga';
+console.log(message);
+
