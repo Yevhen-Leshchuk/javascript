abff5f18618927bde7dac8216c427a410e0d9888
yevhen leshchuk <evg.leschuk@gmail.com>
2021-03-23 22:04:57 +0200

    added tasks-01, 02 to js-02

---
+++ js-02/js/task-02.js
@@ -0,0 +1,49 @@
+// Задание 2
+// Напиши скрипт подсчета стоимости гравировки украшений.
+// Для этого создай функцию calculateEngravingPrice(message, pricePerWord)
+// принимающую строку(в строке будут только слова и пробелы) и цену гравировки одного слова,
+//     и возвращающую цену гравировки всех слов в строке.
+
+const calculateEngravingPrice = function(message, pricePerWord) {
+   
+    const words = message.split(' ');
+    console.log(words, pricePerWord);
+
+    const numbers = [];
+
+    for (let i = 0; i <= words.length; i += 1) {
+        numbers.push(i);
+        let totalPrice = words.length * pricePerWord;
+
+        return `Стоимость гравировки всех слов: ${totalPrice}`;
+    }      
+};
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
+
+
+
+
+
+
+
