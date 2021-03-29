d644ed3dd883e774f760ca03cbdc966f6fa4fee7
yevhen leshchuk <evg.leschuk@gmail.com>
2021-03-29 23:10:01 +0300

    added 3-th variant solution to task-07 to js-02

--- js-02/js/task-07.js
+++ js-02/js/task-07.js
@@ -62,13 +62,12 @@
 // const addLogin = function (allLogins, login) {
 
 //   if (isLoginValid(login) === false) {
-//     return 'Mistake! Login must be from 4 to 16 characters.';
-//   }
-//   if (isLoginUnique(allLogins, login) === false) {
-//     return 'This login is already in use!';
+//     return 'Ошибка! Логин должен быть от 4 до 16 символов';
+//   } else if (isLoginUnique(allLogins, login) === false) {
+//     return 'Такой логин уже используется!';
 //   } else {
 //     logins.push(login);
-//     return 'Login added successfully!';
+//     return 'Логин успешно добавлен!';
 //   }
 // };
 
@@ -80,6 +79,40 @@
 // -----------------------
 
 // V2.
+
+// const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+// const isLoginValid = function (login) {
+//   const loginLength = login.split('');
+
+//   return loginLength.length >= 4 && loginLength.length <= 16 ? true : false;
+// };
+
+// const isLoginUnique = function (allLogins, login) {
+
+//   return allLogins.includes(login) ? false : true;
+// };
+
+// const addLogin = function (allLogins, login) {
+
+//   if (isLoginValid(login) === false) {
+//     return 'Ошибка! Логин должен быть от 4 до 16 символов';
+//   } else if (isLoginUnique(allLogins, login) === false) {
+//     return 'Такой логин уже используется!';
+//   } else {
+//     logins.push(login);
+//     return 'Логин успешно добавлен!';
+//   }
+// };
+
+// console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+// console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+// console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+// console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+
+// -----------------------
+
+// V3.
 
 const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
 
@@ -94,16 +127,19 @@ const isLoginUnique = function (allLogins, login) {
   return allLogins.includes(login) ? false : true;
 };
 
-const addLogin = function (allLogins, login) {
+const addLogin = function (allLogins, login) {    // Паттерн Guard Clause
+
 
   if (isLoginValid(login) === false) {
     return 'Ошибка! Логин должен быть от 4 до 16 символов';
-  } else if (isLoginUnique(allLogins, login) === false) {
-    return 'Такой логин уже используется!';
-  } else {
-    logins.push(login);
-    return 'Логин успешно добавлен!';
   }
+
+  if (isLoginUnique(allLogins, login) === false) {
+    return 'Такой логин уже используется!';
+  }
+
+  logins.push(login);
+  return 'Логин успешно добавлен!';
 };
 
 console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
