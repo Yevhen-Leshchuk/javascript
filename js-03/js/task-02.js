eeb3d322a15fec679b52729ab89a71d4d108baf0
yevhen leshchuk <evg.leschuk@gmail.com>
2021-04-06 00:54:22 +0300

    added task-01, 02 to js-03

---
+++ js-03/js/task-02.js
@@ -0,0 +1,18 @@
+// Задание 2
+// Напиши функцию countProps(obj), считающую кол - во свойств в объекте.
+// Функция возвращает число - количество свойств.
+
+const countProps = function (obj) {
+  const keys = Object.keys(obj);
+  // console.log(keys);
+  /,/
+  return keys.length;
+};
+
+
+console.log(countProps({})); // 0
+
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
+
