71c31807a010d4a1fba95fc8bceb3579b3d51825
yevhen leshchuk <evg.leschuk@gmail.com>
2021-05-15 23:34:45 +0300

    added task-04 to js-07.3

--- js-07.3/js/task-02.js
+++ js-07.3/js/task-02.js
@@ -38,3 +38,4 @@ const array = ingredients.map(ingredient => {
 
 
 
+
---
+++ js-07.3/js/task-04.js
@@ -0,0 +1,33 @@
+// Задание 4
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать значение счетчика на 1.
+
+// Создай переменную counterValue в которой будет хранится текущее значение счетчика.
+// Создай функции increment и decrement для увеличения и уменьшения значения счетчика
+// Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+//   < div id = "counter" >
+//   <button type="button" data-action="decrement">-1</button>
+//   <span id="value">0</span>
+//   <button type="button" data-action="increment">+1</button>
+// </ div >
+
+const incrementBtnRef = document.querySelector('button[data-action="increment"]');
+const decrementBtnRef = document.querySelector('button[data-action="decrement"]');
+const valueRef = document.querySelector('#value');
+
+incrementBtnRef.addEventListener('click', incrementValue);
+
+decrementBtnRef.addEventListener('click', decrementValue);
+
+let counterValue = 0;
+
+function incrementValue() {
+  counterValue += 1;
+  valueRef.textContent = counterValue;
+}
+
+function decrementValue() {
+  counterValue -= 1;
+  valueRef.textContent = counterValue;
+}
+
+
