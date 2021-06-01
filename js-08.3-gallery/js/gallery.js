21b85062b0caf6a92ba12ae8fe0460ba50d026e7
yevhen leshchuk <evg.leschuk@gmail.com>
2021-06-01 22:56:30 +0300

    added modal to js-08.3-gallery

--- js-08.3-gallery/js/gallery.js
+++ js-08.3-gallery/js/gallery.js
@@ -3,29 +3,67 @@ console.log("gallery :", gallery);
 
 
 const galleryRef = document.querySelector('.js-gallery');
-// console.log(galleryRef);
+const openModalRef = document.querySelector('.js-lightbox');
+const closeModalRef = document.querySelector('button[data-action="close-lightbox"]');
+const lightboxImageRef = document.querySelector('.lightbox__image');
+const lightboxRef = document.querySelector('.lightbox__overlay');
+
 const fragment = document.createDocumentFragment();
 
+galleryRef.addEventListener('click', onOpenModal);
+closeModalRef.addEventListener('click', onCloseModal);
+lightboxRef.addEventListener('click', onLightboxClick);
+
 gallery.forEach((el) => {
-  // console.log(el);
   const itemRef = document.createElement("li");
   itemRef.classList.add('gallery__item');
+
   const linkRef = document.createElement("a");
   linkRef.classList.add('gallery__link');
   linkRef.href = el.preview;
-  // console.log(linkRef);
-  const imgRef = document.createElement("img");
-  imgRef.classList.add('gallery__image');
-  imgRef.src = el.original;
-  imgRef.setAttribute("data-source", el.original);
-  imgRef.alt = el.description;
-  // console.log(imgRef);
-  linkRef.appendChild(imgRef);
+
+  const galleryImgRef = document.createElement("img");
+  galleryImgRef.classList.add('gallery__image');
+  galleryImgRef.src = el.original;
+  galleryImgRef.setAttribute("data-source", el.original);
+  galleryImgRef.alt = el.description;
+
+  linkRef.appendChild(galleryImgRef);
   itemRef.appendChild(linkRef);
-  // console.log(itemRef);
+
   fragment.appendChild(itemRef);
 });
 
 galleryRef.appendChild(fragment);
 
-console.log(galleryRef);
+
+function onOpenModal(event) {
+  window.addEventListener('keydown', onPressEscape);
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const imageRef = event.target;
+  const largeImageURL = imageRef.dataset.source;
+  lightboxImageRef.src = largeImageURL;
+  openModalRef.classList.add('is-open');
+}
+
+function onCloseModal() {
+  window.removeEventListener('keydown', onPressEscape);
+  lightboxImageRef.src = '';
+  openModalRef.classList.remove('is-open');
+}
+
+function onLightboxClick(e) {
+  if (e.target === e.currentTarget) {
+    onCloseModal();
+  }
+}
+
+function onPressEscape(e) {
+  if (e.code === "Escape") {
+    onCloseModal();
+  }
+}
+
