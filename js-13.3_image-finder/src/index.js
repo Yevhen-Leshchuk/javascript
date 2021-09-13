f6e36cb05408152ccf6b91a3a95012cb216c493c
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2021-09-13 23:41:58 +0300

    added Load more button to js-13.3

--- js-13.3_image-finder/src/index.js
+++ js-13.3_image-finder/src/index.js
@@ -5,8 +5,26 @@ import updateImagesMarkup from './js/update-images-markup';
 import 'material-icons/iconfont/material-icons.css';
 import './sass/styles.scss';
 
+const loadMoreBtn = {
+  enable() {
+    refs.loadMoreBtn.disabled = false;
+    refs.loadMoreBtnLabel.textContent = 'Load More';
+    refs.loadMoreBtnSpinner.classList.add('is-hidden');
+  },
+  disable() {
+    refs.loadMoreBtn.disabled = true;
+    refs.loadMoreBtnLabel.textContent = 'Loading...';
+    refs.loadMoreBtnSpinner.classList.remove('is-hidden');
+  },
+  show() {
+    refs.loadMoreBtn.classList.remove('is-hidden');
+  },
+};
+
+
 refs.searchForm.addEventListener('submit', searchFormSubmitHandler);
 refs.loadMoreBtn.addEventListener('click', fetchImages);
+
 
 function searchFormSubmitHandler(event) {
   event.preventDefault();
@@ -17,20 +35,35 @@ function searchFormSubmitHandler(event) {
   const form = event.currentTarget;
   apiService.query = form.elements.query.value;
 
-  form.reset();
   clearContainer();
   apiService.resetPage();
+  fetchImages();
+  form.reset();
+}
+
+
+function fetchImages() {
+  loadMoreBtn.disable();
+
   apiService.fetchImages().then(hits => {
     updateImagesMarkup(hits);
+    loadMoreBtn.show();
+    loadMoreBtn.enable();
+    scrollContainer();
   });
 }
 
-function fetchImages() {
-  apiService.fetchImages().then(hits => {
-    updateImagesMarkup(hits);
-  });
-}
 
 function clearContainer() {
   refs.imagesContainer.innerHTML = '';
+}
+
+const galleryImg = document.getElementById('gallery');
+
+
+function scrollContainer() {
+  galleryImg.scrollIntoView({
+    behavior: 'smooth',
+    block: 'end',
+  });
 }
--- js-13.3_image-finder/src/js/refs.js
+++ js-13.3_image-finder/src/js/refs.js
@@ -1,5 +1,7 @@
 const searchForm = document.querySelector('.search-form');
 const imagesContainer = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.btn-primary');
+const loadMoreBtn = document.querySelector('[data-action="load-more"]');
+const loadMoreBtnLabel = document.querySelector('button[data-action = "load-more"]> .label');
+const loadMoreBtnSpinner = document.querySelector('button[data-action = "load-more"]> .spinner');
 
-export default { searchForm, imagesContainer, loadMoreBtn };
+export default { searchForm, imagesContainer, loadMoreBtn, loadMoreBtnLabel, loadMoreBtnSpinner };
