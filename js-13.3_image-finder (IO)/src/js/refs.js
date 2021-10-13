4e51396202ede0c73f50459520659246a388584a
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2021-10-14 00:00:30 +0300

    added fixes to modal

---
+++ js-13.3_image-finder (IO)/src/js/refs.js
@@ -0,0 +1,8 @@
+const refs = {
+  searchForm: document.querySelector('.search-form'),
+  imagesContainer: document.querySelector('.gallery'),
+  modalRef: document.querySelector('.basicLightbox'),
+  bodyRef: document.querySelector('body'),
+};
+
+export default refs;
