02ea6ae97c8f5e113d1c4e8d444aa8c65130c364
Yevhen Leshchuk <evg.leschuk@gmail.com>
2021-10-14 00:22:33 +0300

    Delete js-13.3_image-finder directory

--- js-13.3_image-finder/src/js/refs.js
+++
@@ -1,8 +0,0 @@
-const refs = {
-  searchForm: document.querySelector('.search-form'),
-  imagesContainer: document.querySelector('.gallery'),
-  modalRef: document.querySelector('.basicLightbox'),
-  bodyRef: document.querySelector('body'),
-};
-
-export default refs;
